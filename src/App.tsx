52f557f01f4bc2821d343b8caadf14995dc4360f
deonneon <dtnifti@gmail.com>
2025-03-13 19:28:00 -0500

    update fix

--- src/App.tsx
+++ src/App.tsx
@@ -409,7 +409,7 @@ const App = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
-                <span>{posts[selectedPostIndex].author}</span>
+                <span>{posts[selectedPostIndex].author === "AutoModerator" ? "AutoMod" : posts[selectedPostIndex].author}</span>
               </div>
               
               <div className="flex items-center mr-4">
