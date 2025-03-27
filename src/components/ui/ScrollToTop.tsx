b7a93e9697fff503906db6cd1fb8e2eca645b1e2
deonneon <dtnifti@gmail.com>
2025-03-27 17:43:23 -0600

    improve tap area

--- src/components/ui/ScrollToTop.tsx
+++ src/components/ui/ScrollToTop.tsx
@@ -7,38 +7,39 @@ interface ScrollToTopProps {
 
 const ScrollToTop: FC<ScrollToTopProps> = ({ show, onClick }) => {
   return (
-    <button
-      onClick={onClick}
-      className={`
-        fixed bottom-20 left-2 z-10
-        md:bottom-8 md:right-8
-        ${
-          show
-            ? "opacity-90 scale-100"
-            : "opacity-0 scale-95 pointer-events-none"
-        }
-       hover:bg-orange-600/90 text-gray-300
-        rounded-lg p-2 shadow-xl backdrop-blur-md
-        transition-all duration-200 border border-white/20
-      `}
-      aria-label="Scroll to top"
-      title="Back to top"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-4 w-4 md:h-5 md:w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
+    <div className="fixed bottom-20 left-2 z-10 md:bottom-8 md:right-8">
+      <button
+        onClick={onClick}
+        className={`
+          ${
+            show
+              ? "opacity-90 scale-100"
+              : "opacity-0 scale-95 pointer-events-none"
+          }
+          hover:bg-orange-600/90 text-gray-300
+          rounded-lg p-2 shadow-xl backdrop-blur-md
+          transition-all duration-200 border border-white/20
+          relative before:absolute before:inset-[-12px] before:content-['']
+        `}
+        aria-label="Scroll to top"
+        title="Back to top"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M5 10l7-7m0 0l7 7m-7-7v18"
-        />
-      </svg>
-    </button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4 md:h-5 md:w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M5 10l7-7m0 0l7 7m-7-7v18"
+          />
+        </svg>
+      </button>
+    </div>
   );
 };
 
