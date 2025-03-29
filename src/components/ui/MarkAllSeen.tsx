f6e5b73fb0adc16af4c4db1ec3f6d4da9e7ccd92
deonneon <dtnifti@gmail.com>
2025-03-29 15:31:28 -0500

    fix

--- src/components/ui/MarkAllSeen.tsx
+++ src/components/ui/MarkAllSeen.tsx
@@ -22,7 +22,7 @@ const MarkAllSeen: FC<MarkAllSeenProps> = ({ onClick, hasNewComments = false }) 
   const isDisabled = !hasNewComments && !isChecked;
 
   return (
-    <div className="fixed bottom-8 left-[50%] translate-x-[-50%] z-10 md:bottom-20 md:right-8">
+    <div className="md:hidden fixed bottom-8 left-[50%] translate-x-[-50%] z-10 md:bottom-20 md:right-8">
       <button
         onClick={handleClick}
         className={`opacity-90 ${
