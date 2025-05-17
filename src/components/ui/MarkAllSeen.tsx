1c6065300650123adfd7f0ffc80ef751b083d49a
deonneon <dtnifti@gmail.com>
2025-05-16 22:10:11 -0500

    Update MarkAllSeen component: increase icon width from 5 to 8 for improved visibility and consistency in UI design.

--- src/components/ui/MarkAllSeen.tsx
+++ src/components/ui/MarkAllSeen.tsx
@@ -40,7 +40,7 @@ const MarkAllSeen: FC<MarkAllSeenProps> = ({ onClick, hasNewComments = false }) 
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
+          className="h-5 w-8"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
